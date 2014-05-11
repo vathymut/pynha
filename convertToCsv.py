cb3f771ca3121edcf7605c81e5361a3e64c21fa9
Vathy M. Kamulete <vathymut@gmail.com>
2014-05-11 17:07:22 -0400

    Added pdf_to_csv function.

--- convertToCsv.py
+++ convertToCsv.py
@@ -40,37 +40,39 @@ ISSUER_DIRS = list( gen_issuer_names( skip_list = None ) )
 ##delete_csv_from_loggers( issuers_list = ISSUER_DIRS, logging_folder = 'logging_ppl_amount' )
 
 # CSV info
-HEADERS = [ 'page_no', 'obj_no', 'x0', 'y0', 'x1', 'y1', 'text' ]
 PAGES_TO_PARSE_FIRSTPAGE = ( 0, 1, 2, 3 )
 PAGES_TO_PARSE_RAW = ( 5, 6, 7, 8 )
 
+def pdf_to_csv( pdf_dir = PDFDIR, \
+                csv_dir = CSVDIR_FIRSTPAGE, \
+                keep_issuers = ISSUER_DIRS, \
+                pages_to_parse = PAGES_TO_PARSE_FIRSTPAGE ):
+    '''
+    Convert pdf files in pdf_dir to csv for the pages in pages_to_parse.
+    Converted csv files are saved in csv_dir.
+    Convert only files belonging to issuers in keep_issuers.
+    '''
+    HEADERS = [ 'page_no', 'obj_no', 'x0', 'y0', 'x1', 'y1', 'text' ]
+    chdir( pdf_dir )
+    failed_files = []
+    args = dict( pdf_dir = pdf_dir, csv_dir = csv_dir, keep_issuers = keep_issuers )
+    for csv_file, pdf_file in gen_pdf_to_csv( **args ):
+        print csv_file, pdf_file
+        try:
+            writecsv_to_path( csv_file, pdf_file, HEADERS, pages_to_parse )
+        except Exception, err:
+            print '\tProblem with file: %s' % ( csv_file )
+            failed_files.append( csv_file )
+        print 'in directory:' , getcwd( )
+    print failed_files
+    return failed_files
+
 if __name__ == '__main__':
     ######### CSVDIR_FIRSTPAGE #########
-    chdir( PDFDIR )
-    failed_files = []
-    args = dict( pdf_dir = PDFDIR, csv_dir = CSVDIR_FIRSTPAGE, keep_issuers = ISSUER_DIRS )
-    for csv_file, pdf_file in gen_pdf_to_csv( **args ):
-        print csv_file, pdf_file
-        try:
-			writecsv_to_path( csv_file, pdf_file, HEADERS, PAGES_TO_PARSE_FIRSTPAGE )
-		# TO DO: Replace except Exception hack
-        except Exception, err:
-			print '\tProblem with file: %s' % ( csv_file )
-			failed_files.append( csv_file )
-        print 'in directory:' , getcwd( )
-    print failed_files
+    pdf_to_csv( pdf_dir = PDFDIR, csv_dir = CSVDIR_FIRSTPAGE, \
+                keep_issuers = ISSUER_DIRS, \
+                pages_to_parse = PAGES_TO_PARSE_FIRSTPAGE )
     ######### CSVDIR_RAW #########
-    chdir( PDFDIR )
-    failed_files = []
-    args = dict( pdf_dir = PDFDIR, csv_dir = CSVDIR_RAW, keep_issuers = ISSUER_DIRS )
-    for csv_file, pdf_file in gen_pdf_to_csv( **args ):
-        print csv_file, pdf_file
-        try:
-			writecsv_to_path( csv_file, pdf_file, HEADERS, PAGES_TO_PARSE_RAW )
-		# TO DO: Replace except Exception hack
-        except Exception, err:
-			print '\tProblem with file: %s' % ( csv_file )
-			failed_files.append( csv_file )
-        print 'in directory:' , getcwd( )
-    print failed_files
-
+    pdf_to_csv( pdf_dir = PDFDIR, csv_dir = CSVDIR_RAW, \
+            keep_issuers = ISSUER_DIRS, \
+            pages_to_parse = PAGES_TO_PARSE_RAW )
